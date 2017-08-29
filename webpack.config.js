8d7d361f41b73eec80281e6e6ad5edcdaa63aae4
Ganesha <reekoheek@gmail.com>
2017-08-29 15:23:10 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function (env) {
+  return {
+    context: path.join(__dirname, 'src'),
+    entry: {
+      index: './index.js',
+    },
+    output: {
+      path: path.join(__dirname, 'www'),
+      filename: 'lib/[name].js',
+    },
+    devtool: 'sourcemap',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: getBabelLoader(env),
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    ],
+  };
+};
+
+function getBabelLoader () {
+  let plugins = [
+    'syntax-dynamic-import',
+    // require.resolve('babel-plugin-transform-async-to-generator'),
+    // [ require.resolve('babel-plugin-__coverage__'), { 'ignore': 'node_modules' } ],
+    // require.resolve('babel-plugin-syntax-dynamic-import'),
+    // require.resolve('babel-plugin-istanbul')
+  ];
+
+  let presets = [
+    // require.resolve('babel-preset-es2015'),
+    // require.resolve('babel-preset-stage-3'),
+  ];
+
+  return {
+    loader: 'babel-loader',
+    options: {
+      babelrc: false,
+      plugins,
+      presets,
+      cacheDirectory: true,
+    },
+  };
+}
